029d1a12be83770af3b08d50a408aeeccc484bc5
Sudan bhandari <54224447+sudan45@users.noreply.github.com>
2020-10-04 07:54:38 +0545

    Update models.py
    admin readable

--- employee/models.py
+++ employee/models.py
@@ -6,6 +6,9 @@ class Employee(models.Model):
     ename = models.CharField(max_length=100)
     eemail = models.EmailField()
     econtact = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.ename
 
     class Meta:
         db_table = "employee"
